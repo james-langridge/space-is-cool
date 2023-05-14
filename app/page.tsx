eb3db539ccf251bd5d56b0e90e961e6e3d9b86bf
James <james.d.langridge@gmail.com>
2023-05-14 12:05:31 -0500

    Replace homepage with placeholder

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 
-import PhotoOfTheDay from '@/components/PhotoOfTheDay'
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-12">
       <h1 className="prose text-6xl p-5">Rover Photos</h1>
-      <PhotoOfTheDay />
+      <div>TODO: add documentation</div>
     </main>
   )
 }
