77dcfb660af55d468dfa053c4664e9f063567cea
James <james.d.langridge@gmail.com>
2023-05-14 15:54:42 -0500

    Decrease btn padding on mobile

--- components/RoverButtonGroup.tsx
+++ components/RoverButtonGroup.tsx
@@ -16,7 +16,7 @@ export default function RoverButtonGroup() {
             key={rover}
             disabled={rover === form.rover}
             className={classNames(
-              'px-4 py-2 text-sm font-medium text-gray-600 transition-colors duration-200 sm:text-base sm:px-6  dark:text-gray-300',
+              'px-2.5 py-2 text-sm font-medium text-gray-600 transition-colors duration-200 sm:text-base sm:px-6 dark:text-gray-300',
               rover === form.rover
                 ? 'bg-gray-200 dark:bg-gray-500'
                 : 'hover:bg-gray-100 dark:hover:bg-gray-800',
