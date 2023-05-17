e2115017d14c205f072ebd7a21a7d909f9e03167
James <james.d.langridge@gmail.com>
2023-05-16 22:10:28 -0500

    Update API documentation

--- lib/api.ts
+++ lib/api.ts
@@ -42,29 +42,36 @@ export const getMissionManifest = async (
 }
 
 /**
- * @typedef {Object} PhotoDate - Represents a date on Mars or Earth.
- * @property {('sol' | 'earth_date')} type - The type of the date ('sol' or 'earth_date').
- * @property {string} date - The date string. For 'sol', it can range from 0 (date of landing) up to the current maximum in the database. For 'earth_date', it should be formatted as 'yyyy-mm-dd'. The earliest date available is the date of landing for each rover.
- */
-export type PhotoDate = {
-  type: 'sol' | 'earth_date'
-  date: string
-}
-
-/**
- * @typedef {Object} GetPhotosSearchParams - The search parameters for the /photos endpoint.
- * @property {RoverName} rover - The name of the rover.
- * @property {PhotoDate} date - The date on Mars or Earth.
- * @property {(CameraName | CameraNamePerseverance)} camera - The camera name. Note that different rovers have different cameras: https://github.com/corincerami/mars-photo-api#cameras
- * @property {string} page - The default response returns all photos. The page parameter can be specified and returns 25 photos per page.
+ * The search parameters for the /photos endpoint.
  */
 export type GetPhotosSearchParams = {
+  /** The rover's name */
   rover: RoverName
+
+  /** The type of the date: 'sol' or 'earth_date' */
   dateType: 'sol' | 'earth_date'
-  page?: number
+
+  /**
+   * The Martian rotation or day on which the photo was taken, counting up from the rover's landing date.
+   * A photo taken on Curiosity's 1000th Martian sol exploring Mars, for example, will have a sol attribute of 1000.
+   * This is required if dateType === 'sol'.
+   */
   sol?: number | ''
+
+  /** The Earth date on which the photo was taken. This is required if dateType === 'earth_date'. */
   earth_date?: string
+
+  /**
+   * The camera name. Note that different rovers have different cameras: https://github.com/corincerami/mars-photo-api#cameras
+   * This is optional.
+   */
   camera?: CameraName | ''
+
+  /**
+   * The default response returns all photos. The page parameter can be specified and returns 25 photos per page.
+   * This is optional.
+   */
+  page?: number
 }
 
 export const getPhotos = async (
