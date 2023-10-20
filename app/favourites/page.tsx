23377b2214164c13e0e094541f789f4b7dcc73ad
jlangridge <l@ngridge.com>
2023-10-20 12:00:41 +0200

    Refactor /favourites page

--- app/favourites/page.tsx
+++ app/favourites/page.tsx
@@ -3,24 +3,11 @@
 import React from 'react'
 import {useReadLocalStorage} from 'usehooks-ts'
 
-import Container from '@/components/shared/Container'
-import Header from '@/components/shared/Header'
-import PhotoGrid from '@/components/shared/PhotoGrid'
-import PhotoThumbnail from '@/components/shared/PhotoThumbnail'
+import FavouritesPage from '@/components/pages/favourites/FavouritesPage'
 import {PhotoWithPage} from '@/lib/api'
 
 export default function FavouritePhotos() {
   const favourites = useReadLocalStorage<PhotoWithPage[]>('favourites')
 
-  return (
-    <Container>
-      <Header string="Favourite Photos" />
-      <PhotoGrid>
-        {favourites &&
-          favourites.map(photo => (
-            <PhotoThumbnail key={photo.id} photo={photo} mode="favourite" />
-          ))}
-      </PhotoGrid>
-    </Container>
-  )
+  return <FavouritesPage data={favourites} />
 }
---
+++ components/pages/favourites/FavouritesPage.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import PhotoGrid from '@/components/shared/PhotoGrid'
+import PhotoThumbnail from '@/components/shared/PhotoThumbnail'
+import {PhotoWithPage} from '@/lib/api'
+
+export interface FavouritesPageProps {
+  data: PhotoWithPage[] | null
+}
+
+export default function FavouritesPage({data}: FavouritesPageProps) {
+  return (
+    <PhotoGrid>
+      {data &&
+        data.map(photo => (
+          <PhotoThumbnail key={photo.id} photo={photo} mode="favourite" />
+        ))}
+    </PhotoGrid>
+  )
+}
