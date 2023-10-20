c1eafc46a8d708eb2f997eda3e51cfe56f14b31a
jlangridge <l@ngridge.com>
2023-10-20 12:38:35 +0200

    Refactor /photo route

--- app/photo/[id]/page.tsx
+++ app/photo/[id]/page.tsx
@@ -1,19 +1,8 @@
-'use client'
-
-import {useState} from 'react'
-import {useMediaQuery} from 'usehooks-ts'
-
-import ButtonBack from '@/components/pages/photo/ButtonBack'
-import ButtonInfo from '@/components/pages/photo/ButtonInfo'
-import ButtonNext from '@/components/pages/photo/ButtonNext'
-import ButtonPrev from '@/components/pages/photo/ButtonPrev'
-import PhotoSidebar from '@/components/pages/photo/PhotoSidebar'
-import PhotoSwipe from '@/components/pages/photo/PhotoSwipe'
-import ButtonFavourite from '@/components/shared/ButtonFavourite'
-import {usePhotoHandler} from '@/hooks'
+import PhotoPage from '@/components/pages/photo/PhotoPage'
 import {RoverName} from '@/types/APIResponseTypes'
 
 export type Params = {id: string}
+
 export type SearchParams = {
   rover: RoverName
   favourite: string
@@ -28,57 +17,5 @@ type Props = {
 }
 
 export default function Page({params, searchParams}: Props) {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
-  const {photo, getNextPhoto, getPrevPhoto} = usePhotoHandler({
-    params,
-    searchParams,
-  })
-  const isMobile = useMediaQuery('(max-width: 640px)')
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen)
-  }
-
-  if (!photo) {
-    return null
-  }
-
-  // The Next.js Image Component requires width and height props,
-  // but we don't know them in advance.
-  // The fill prop causes the image to fill the parent element instead of setting width and height,
-  // but we still need to set the height of the parent element.
-  // So using the <img> element here.
-  return (
-    <div className="relative h-screen bg-black flex items-center justify-center dark:invert">
-      <PhotoSwipe
-        getNextPhoto={getNextPhoto}
-        getPrevPhoto={getPrevPhoto}
-        toggleSidebar={toggleSidebar}
-        isSidebarOpen={isSidebarOpen}
-      >
-        <ButtonBack />
-        <ButtonFavourite
-          photo={photo}
-          position={isMobile ? 'top-2 right-2' : 'top-2 right-16'}
-        />
-        {!isMobile && (
-          <>
-            <ButtonInfo onClick={toggleSidebar} />
-            <ButtonPrev onClick={getPrevPhoto} />
-            <ButtonNext onClick={getNextPhoto} />
-          </>
-        )}
-        <PhotoSidebar
-          isOpen={isSidebarOpen}
-          onClose={toggleSidebar}
-          photo={photo}
-        />
-        <img
-          src={photo.img_src}
-          alt={String(photo.id)}
-          className="max-h-full max-w-full"
-        />
-      </PhotoSwipe>
-    </div>
-  )
+  return <PhotoPage data={{params, searchParams}} />
 }
---
+++ components/pages/photo/PhotoPage.tsx
@@ -0,0 +1,75 @@
+'use client'
+
+import {useState} from 'react'
+import {useMediaQuery} from 'usehooks-ts'
+
+import {Params, SearchParams} from '@/app/photo/[id]/page'
+import ButtonBack from '@/components/pages/photo/ButtonBack'
+import ButtonInfo from '@/components/pages/photo/ButtonInfo'
+import ButtonNext from '@/components/pages/photo/ButtonNext'
+import ButtonPrev from '@/components/pages/photo/ButtonPrev'
+import PhotoSidebar from '@/components/pages/photo/PhotoSidebar'
+import PhotoSwipe from '@/components/pages/photo/PhotoSwipe'
+import ButtonFavourite from '@/components/shared/ButtonFavourite'
+import {usePhotoHandler} from '@/hooks'
+
+type PhotoPageProps = {
+  data: {params: Params; searchParams: SearchParams}
+}
+
+export default function PhotoPage({data}: PhotoPageProps) {
+  const {params, searchParams} = data
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+  const {photo, getNextPhoto, getPrevPhoto} = usePhotoHandler({
+    params,
+    searchParams,
+  })
+  const isMobile = useMediaQuery('(max-width: 640px)')
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen)
+  }
+
+  if (!photo) {
+    return null
+  }
+
+  // The Next.js Image Component requires width and height props,
+  // but we don't know them in advance.
+  // The fill prop causes the image to fill the parent element instead of setting width and height,
+  // but we still need to set the height of the parent element.
+  // So using the <img> element here.
+  return (
+    <div className="relative flex h-screen items-center justify-center bg-black dark:invert">
+      <PhotoSwipe
+        getNextPhoto={getNextPhoto}
+        getPrevPhoto={getPrevPhoto}
+        toggleSidebar={toggleSidebar}
+        isSidebarOpen={isSidebarOpen}
+      >
+        <ButtonBack />
+        <ButtonFavourite
+          photo={photo}
+          position={isMobile ? 'top-2 right-2' : 'top-2 right-16'}
+        />
+        {!isMobile && (
+          <>
+            <ButtonInfo onClick={toggleSidebar} />
+            <ButtonPrev onClick={getPrevPhoto} />
+            <ButtonNext onClick={getNextPhoto} />
+          </>
+        )}
+        <PhotoSidebar
+          isOpen={isSidebarOpen}
+          onClose={toggleSidebar}
+          photo={photo}
+        />
+        <img
+          src={photo.img_src}
+          alt={String(photo.id)}
+          className="max-h-full max-w-full"
+        />
+      </PhotoSwipe>
+    </div>
+  )
+}
