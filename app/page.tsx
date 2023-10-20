f1c820a59acaa521f71d53b3731d4372d6303e59
jlangridge <l@ngridge.com>
2023-10-20 12:49:09 +0200

    Refactor home route

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,7 @@
-import Link from 'next/link'
 import React from 'react'
 
-import Header from '@/components/shared/Header'
+import HomePage from '@/components/pages/home/HomePage'
 
 export default function Home() {
-  return (
-    <main className="container mx-auto px-7 flex flex-col prose min-h-screen">
-      <Header string="SpaceI sCool" />
-      <p>
-        This is a user interface to view, search, and save photos taken by
-        NASA&apos;s rovers on Mars, which are made public via the{' '}
-        <Link href="https://api.nasa.gov/#mars-rover-photos" target="_blank">
-          Mars Rovers Photos API
-        </Link>
-        .
-      </p>
-      <p>
-        <Link href={'/latest-photos'}>View the latest photos</Link> taken by
-        each rover.
-      </p>
-      <p>
-        <Link href={'/search'}>Search for photos</Link> by rover, date, and
-        camera.
-      </p>
-      <p>
-        <Link href={'/favourites'}>View favourite photos</Link>, saved to local
-        browser storage.
-      </p>
-      <p>
-        <Link href={'/manifests'}>View the mission manifests</Link> for each
-        rover.
-      </p>
-    </main>
-  )
+  return <HomePage />
 }
