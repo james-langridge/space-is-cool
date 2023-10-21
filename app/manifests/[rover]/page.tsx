fe8bd80b0d4474f05b86f9e16d6612a73856bec3
jlangridge <l@ngridge.com>
2023-10-21 12:10:21 +0200

    Refactor /manifests/[rover]/page

--- app/manifests/[rover]/page.tsx
+++ app/manifests/[rover]/page.tsx
@@ -1,8 +1,8 @@
+import {notFound} from 'next/navigation'
 import React from 'react'
 
 import ManifestsRoverPage from '@/components/pages/manifests/[rover]/ManifestsRoverPage'
-import {getMissionManifest} from '@/lib/api'
-import {RoverName} from '@/types/APIResponseTypes'
+import {PhotoManifest, RoverName} from '@/types/APIResponseTypes'
 
 // Revalidate the cache once a day
 export const revalidate = 86400
@@ -15,9 +15,25 @@ export async function generateStaticParams() {
   }))
 }
 
+const getMissionManifest = async (rover: RoverName): Promise<PhotoManifest> => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/manifests/${rover}?api_key=${String(
+      process.env.NEXT_PUBLIC_API_KEY,
+    )}`,
+  )
+
+  const {photo_manifest} = await res.json()
+
+  return photo_manifest
+}
+
 export default async function Page({params}: {params: {rover: RoverName}}) {
   const {rover} = params
   const manifest = await getMissionManifest(rover)
+
+  if (!manifest) {
+    return notFound()
+  }
 
   return <ManifestsRoverPage data={manifest} />
 }
