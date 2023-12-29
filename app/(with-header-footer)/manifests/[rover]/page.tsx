e4cfb0d7bfb5d05916ff015a6adeea1bd9755fb4
jlangridge <l@ngridge.com>
2023-12-29 15:56:56 -0600

    Add help text to /manifests/[rover]

--- app/(with-header-footer)/manifests/[rover]/page.tsx
+++ app/(with-header-footer)/manifests/[rover]/page.tsx
@@ -21,23 +21,38 @@ export default async function Page({params}: {params: {rover: RoverName}}) {
     <>
       <HeaderNavButtons />
       <Separator className="my-4" />
-      <div className="flex flex-wrap justify-center gap-4 capitalize">
-        {Object.entries(manifest).map(([key, value]) => {
-          if (key !== 'photos') {
-            if (key === 'max_sol' || key === 'total_photos') {
-              value = new Intl.NumberFormat().format(value as number)
-            }
+      <div className="mx-auto space-y-8 px-4">
+        <p className="text-muted-foreground">
+          Click on the Total Photos card to view a searchable table of all the
+          rover&apos;s photos.
+        </p>
+        <div className="flex flex-wrap justify-center gap-4 capitalize">
+          {Object.entries(manifest).map(([key, value]) => {
+            if (key !== 'photos') {
+              if (key === 'max_sol' || key === 'total_photos') {
+                value = new Intl.NumberFormat().format(value as number)
+              }
 
-            if (
-              key === 'max_date' ||
-              key === 'launch_date' ||
-              key === 'landing_date'
-            ) {
-              value = format(new Date(value as string), 'do MMM yyyy')
-            }
+              if (
+                key === 'max_date' ||
+                key === 'launch_date' ||
+                key === 'landing_date'
+              ) {
+                value = format(new Date(value as string), 'do MMM yyyy')
+              }
 
-            return key === 'total_photos' ? (
-              <Link href={`/manifests/${manifest.name}/photos`}>
+              return key === 'total_photos' ? (
+                <Link href={`/manifests/${manifest.name}/photos`}>
+                  <Card key={value as string}>
+                    <CardHeader>
+                      <CardTitle>{key.split('_').join(' ')}</CardTitle>
+                    </CardHeader>
+                    <CardContent className="text-center">
+                      {value as string | number}
+                    </CardContent>
+                  </Card>
+                </Link>
+              ) : (
                 <Card key={value as string}>
                   <CardHeader>
                     <CardTitle>{key.split('_').join(' ')}</CardTitle>
@@ -46,19 +61,10 @@ export default async function Page({params}: {params: {rover: RoverName}}) {
                     {value as string | number}
                   </CardContent>
                 </Card>
-              </Link>
-            ) : (
-              <Card key={value as string}>
-                <CardHeader>
-                  <CardTitle>{key.split('_').join(' ')}</CardTitle>
-                </CardHeader>
-                <CardContent className="text-center">
-                  {value as string | number}
-                </CardContent>
-              </Card>
-            )
-          }
-        })}
+              )
+            }
+          })}
+        </div>
       </div>
     </>
   )
